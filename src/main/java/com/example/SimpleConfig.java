360ee0cb09e0fb0711b645f11ab6787662aa47e3
93exe <dimapon993@gmail.com>
2026-03-19 06:10:11 +0300

    Create SimpleConfig.java

---
+++ src/main/java/com/example/SimpleConfig.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class SimpleConfig {
+    // Позиция
+    public static float x = 0.2f;
+    public static float y = -0.3f;
+    public static float z = -0.1f;
+    
+    // Анимация (сила плавности)
+    public static float swingSpeed = 1.0f;
+    public static float rotationStrength = 20.0f;
+}
